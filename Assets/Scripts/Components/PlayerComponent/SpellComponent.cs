465fff00268b317a50b7f5714ecdaad9311cc03e
DamienP1602 <148540742+DamienP1602@users.noreply.github.com>
2025-04-12 13:17:47 +0200

    ooof

--- Assets/Scripts/Components/PlayerComponent/SpellComponent.cs
+++ Assets/Scripts/Components/PlayerComponent/SpellComponent.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.PackageManager.UI;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
 
 public class SpellComponent : MonoBehaviour
 {
